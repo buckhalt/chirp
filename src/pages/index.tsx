8e208deecfa3dd4ba865ac0860e69446d1f976d6
halaeb <halaeb@gmail.com>
2023-03-30 11:09:13 -0500

    Next image tag, style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,27 @@ import { type NextPage } from "next";
 import { SignInButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
 
-import { api, RouterOutputs } from "~/utils/api";
+import { api } from "~/utils/api";
+import type { RouterOutputs } from "~/utils/api";
+
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
+
+dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const {user} = useUser();
 
-  console.log(user);
-
   if (!user) return null;
 
   return <div className="flex gap-3 w-full">
-    <img 
+    <Image 
       src={user.profileImageUrl}
       alt="Profile image"
       className="w-14 h-14 rounded-full"
+      width={56}
+      height={56}
     />
     <input placeholder="Type some emojis!" className="grow bg-transparent outline-none"/>
   </div>
@@ -26,10 +33,19 @@ const PostView = (props: PostWithUser) => {
   const {post, author} = props;
   return (
     <div key={post.id} className="flex gap-3 p-4 border-b border-slate-400">
-      <img src={author.profileImageUrl} className="w-14 h-14 rounded-full"/>
+      <Image
+        src={author.profileImageUrl}
+        alt={`@${author.username}'s profile image`}
+        className="w-14 h-14 rounded-full"
+        width={56}
+        height={56}
+      />
       <div className="flex flex-col">
-        <div className="flex text-slate-300">
+        <div className="flex gap-1 text-slate-300">
           <span>{`@${author.username}`}</span>
+          <span className="font-thin">
+            {`· ${dayjs(post.createdAt).fromNow()}`}
+          </span>
         </div>
         <span>{post.content}</span>
       </div>
