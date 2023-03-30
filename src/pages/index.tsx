bf506a388192ddd9565ea0d58b3d194a8331ffcf
halaeb <halaeb@gmail.com>
2023-03-30 15:07:00 -0500

    working posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,14 @@ const CreatePostWizard = () => {
 
   const [input, setInput] = useState("");
 
-  const { mutate } = api.posts.create.useMutation();
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   if (!user) return null;
 
@@ -36,6 +43,7 @@ const CreatePostWizard = () => {
       type="text"
       value={input}
       onChange={(e) => setInput(e.target.value)}
+      disabled={isPosting}
     />
     <button
       onClick={()=> mutate({ content: input })}>Post</button>
